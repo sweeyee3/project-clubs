b22b6c928e13d563d9afda41907086a24f085675
brandonswee <brandon.swee@thedoodlepeople.com>
2023-03-02 16:04:02 +0800

    added moving hoops forward and backwards

--- team-clubs/Assets/Scripts/MovingHoop.cs
+++ team-clubs/Assets/Scripts/MovingHoop.cs
@@ -15,56 +15,32 @@ public class MovingHoop : MonoBehaviour
     [SerializeField] private int m_moveUnits = 1;
     [SerializeField] private float m_moveSpeed = 2.0f;
 
+    [SerializeField] public Vector3 m_cellIndexTarget;
+    [SerializeField] public Vector3 m_cellIndexOriginal;
+
     private Vector3 m_positionTarget;
     private Vector3 m_positionOriginal;    
 
     private bool m_isMovingInPositiveDirection = true;
 
-    public Vector3 PositionTarget
+    public int MoveUnits
     {
         get
         {
-            return m_positionTarget;
+            return m_moveUnits;
         }
-    }
+    }   
 
     private void Start()
-    {
-        Vector3 gridIndex = m_hoop.CellIndex;
-        Vector3 cellCounts = SpawnManager.Instance.GetCellCounts();
-
-        int targetTargetX = (int)m_hoop.CellIndex.x;
-        int targetTargetY = (int)m_hoop.CellIndex.y;
-        int targetTargetZ = (int)m_hoop.CellIndex.z;
-
+    {        
         int targetOriginalX = (int)m_hoop.CellIndex.x;
         int targetOriginalY = (int)m_hoop.CellIndex.y;
-        int targetOriginalZ = (int)m_hoop.CellIndex.z;
+        int targetOriginalZ = (int)m_hoop.CellIndex.z;        
 
-        int moveDirection = Random.Range(0, 2) == 1 ? -1 : 1;
+        m_cellIndexOriginal = m_hoop.CellIndex;
+        m_cellIndexTarget = GetTargetCellIndex(m_hoop.HoopType, m_hoop.CellIndex, m_moveUnits);
 
-        switch (m_hoop.HoopType)
-        {
-            case Hoop.EHoopType.MOVE_X:
-                var targetX = targetTargetX + (moveDirection * m_moveUnits);
-                Debug.Log(targetX + ", " + cellCounts.x);
-                moveDirection = (targetX < 0 || targetX >= cellCounts.x) ? -moveDirection : moveDirection;
-
-                targetTargetX = Mathf.Clamp(targetTargetX + (moveDirection * m_moveUnits), 0, (int)cellCounts.x);                
-                break;
-            case Hoop.EHoopType.MOVE_Y:
-                var targetY = targetTargetY + (moveDirection * m_moveUnits);
-                moveDirection = (targetY < 0 || targetY >= cellCounts.x) ? -moveDirection : moveDirection;
-                targetTargetY = Mathf.Clamp(targetTargetY + (moveDirection * m_moveUnits), 0, (int)cellCounts.x);                
-                break;
-            case Hoop.EHoopType.MOVE_Z:
-                var targetZ = targetTargetZ + (moveDirection * m_moveUnits);
-                moveDirection = (targetZ < 0 || targetZ >= cellCounts.x) ? -moveDirection : moveDirection;
-                targetTargetZ = Mathf.Clamp(targetTargetZ + (moveDirection * m_moveUnits), 0, (int)cellCounts.x);
-                break;
-        }
-        
-        m_positionTarget = SpawnManager.Instance.GetCellPosition(targetTargetX, targetTargetY, targetTargetZ);
+        m_positionTarget = SpawnManager.Instance.GetCellPosition((int)m_cellIndexTarget.x, (int)m_cellIndexTarget.y, (int)m_cellIndexTarget.z);
         m_positionOriginal = SpawnManager.Instance.GetCellPosition(targetOriginalX, targetOriginalY, targetOriginalZ);
     }
 
@@ -88,4 +64,37 @@ public class MovingHoop : MonoBehaviour
             m_isMovingInPositiveDirection = !m_isMovingInPositiveDirection;
         }        
     }
+
+    public static Vector3 GetTargetCellIndex(Hoop.EHoopType type, Vector3 cellIndex, int moveUnits)
+    {
+        Vector3 gridIndex = cellIndex;
+        Vector3 cellCounts = SpawnManager.Instance.GetCellCounts();
+
+        int targetTargetX = (int)cellIndex.x;
+        int targetTargetY = (int)cellIndex.y;
+        int targetTargetZ = (int)cellIndex.z;       
+
+        int moveDirection = Random.Range(0, 2) == 1 ? -1 : 1;
+
+        switch (type)
+        {
+            case Hoop.EHoopType.MOVE_X:
+                var targetX = targetTargetX + (moveDirection * moveUnits);
+                moveDirection = (targetX < 0 || targetX >= cellCounts.x) ? -moveDirection : moveDirection;
+                targetTargetX = Mathf.Clamp(targetTargetX + (moveDirection * moveUnits), 0, (int)cellCounts.x);
+                break;
+            case Hoop.EHoopType.MOVE_Y:
+                var targetY = targetTargetY + (moveDirection * moveUnits);
+                moveDirection = (targetY < 0 || targetY >= cellCounts.x) ? -moveDirection : moveDirection;
+                targetTargetY = Mathf.Clamp(targetTargetY + (moveDirection * moveUnits), 0, (int)cellCounts.x);
+                break;
+            case Hoop.EHoopType.MOVE_Z:
+                var targetZ = targetTargetZ + (moveDirection * moveUnits);
+                moveDirection = (targetZ < 0 || targetZ >= cellCounts.x) ? -moveDirection : moveDirection;
+                targetTargetZ = Mathf.Clamp(targetTargetZ + (moveDirection * moveUnits), 0, (int)cellCounts.x);
+                break;
+        }
+
+        return new Vector3(targetTargetX, targetTargetY, targetTargetZ);
+    }    
 }
